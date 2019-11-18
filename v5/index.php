685dca0ed6072746c9b846e17217a8770db5f61e
user <sh.daewoong@navercorp.com>
2019-11-18 12:30:42 +0900

    sdfasdfasdf

---
+++ v5/index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <title>DAEWOONG KIM</title>
+
+    <style>
+        .tabs li.is-active a {
+            border-bottom-color: #ffdd57;
+            border-bottom-width: 2px;
+            color: rgba(0, 0, 0, .7);
+            font-weight: bold;
+        }
+
+        .tabs li a:hover {
+            border-bottom-color: #ffdd57;
+            border-bottom-width: 2px;
+        }
+    </style>
+</head>
+<body>
+
+<section class="hero is-medium is-warning">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title">
+                Hi there!
+            </h1>
+            <h2 class="subtitle" id="app">
+                I am looking around to find the right thing to fill me up.
+            </h2>
+        </div>
+    </div>
+</section>
+<section class="section">
+    <div class="container is-fullhd">
+        <div class="columns">
+            <div class="column is-one-fifths">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Intro
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <strong>DAEWOONG KIM</strong>
+                            <br>
+                            <small>Full-Stack Developer</small>
+                            <br>
+                            Javascript, React JS, Java, Spring, MySQL, Redis, Storm
+                        </div>
+                        <a href="https://www.instagram.com/sh_chuck/">
+                        <figure class="image is-32x32">
+                            <img src="https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png" alt="Image">
+                        </figure>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-four-fifths">
+                <div class="tabs">
+                    <ul>
+                        <li class="is-active"><a>Timeline</a></li>
+                        <li><a>Projects</a></li>
+                        <li><a>Videos</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img src="image/wavlogo.png" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>WAV Media</strong> <small>2017 ~ Currently</small>
+                                    <br>
+                                    Full-Stack Developer <br>
+                                    Web, API, Real-time Processing, CMS, Batch <br>
+                                    Javascript, React JS, Java, Spring, MySQL, Redis, Storm
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img src="image/ksign.png" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>KSign</strong> <small>2016 ~ 2017</small>
+                                    <br>
+                                    Back-End Developer <br>
+                                    SSO (Single Sign-On), EAM (Enterprise Asset Management)<br>
+                                    Java
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img src="image/mnsu.jpg" class="isr" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>Minnesota State University, Mankato</strong> <small>2014</small>
+                                    <br>
+                                    Bachelor of Science in Computer Information Technology<br>
+                                    Minor degree in Mathematics
+                                    <br>
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img src="image/mnsu.jpg" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>Academic Computer Center</strong> <small>2013 ~ 2014</small>
+                                    <br>
+                                    STUDENT CONSULTANT
+                                    <br>
+                                    Help customers who have a trouble with anything related to computers.<br>
+                                    IT Solutions on the first floor of Wissink Hall.
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64" style="line-height: 100px; vertical-align: middle">
+                                <img src="image/testenc.jpg" alt="Image" style="vertical-align: middle; line-height: 100px">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>TEST ENC</strong> <small>Summer of 2013</small>
+                                    <br>
+                                    Android Application Tester
+                                    <br>
+                                    Created thousands of test cases. <br>
+                                    Run functional, regression, and alpha testing.
+                                    <br>
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+                <div class="box">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img src="image/mnsu.jpg" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong> IT Solutions</strong> <small>2012 ~ 2013</small>
+                                    <br>
+                                    STUDENT CONSULTANT
+                                    <br>
+                                    Help customers who have a trouble with anything related to computers. <br>
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column is-one-fifths">
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer" style="background-color: #ffdd57; padding: 1rem 1.5rem 1rem;">
+    <div class="content has-text-centered">
+        <p>
+            <a href="https://bulma.io">
+                <img src="./image/made-with-bulma--semiblack.png" alt="Made with Bulma" width="128" height="24">
+            </a>
+        </p>
+    </div>
+</footer>
+</body>
+<script src="./js/typewriter.js"></script>
+<script>
+
+    /*https://github.com/tameemsafi/typewriterjs*/
+    var app = document.getElementById('app');
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+    typewriter.typeString('I am DAEWOONG KIM.')
+        .pauseFor(2500)
+        .deleteChars(13)
+        .typeString('a full stack developer.')
+        .pauseFor(2500)
+        .deleteChars(23)
+        .typeString('looking around to find the right thing to fill me up.')
+        .pauseFor(2500)
+        .start();
+</script>
+</html>
