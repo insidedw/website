57e9fc50054973973cae7bd5acf0ff0999990e12
insidedw <insidedw@gmail.com>
2019-11-27 23:33:50 +0900

    react

---
+++ v6/src/components/TimelineBox.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class TimelineBox extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        const {exp} = this.props;
+        return <div className="box timeline">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={exp.image} alt="Image"/>
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <p>
+                            <strong>{exp.title}</strong><br/>
+                            <small>{exp.period}</small><br/>
+                            {exp.place}<br/>
+                            {exp.desc}
+                        </p>
+                    </div>
+                    <nav className="level is-mobile">
+                        <div className="level-left">
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </div>;
+    }
+}
+
+export default TimelineBox;
