c3d5aed2cde50eac84359cb3a56703be69d6fea5
user <sh.daewoong@navercorp.com>
2019-12-02 17:31:11 +0900

    react test

---
+++ v6/src/components/ProjectBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class ProjectBox extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+    render() {
+        const {exp} = this.props;
+        return <div className="box timeline">
+            <article className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img src={`${exp.image}`} alt={exp.location}/>
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <div className="content">
+                        <p>
+                            <strong>{exp.title}</strong><br/>
+                            {exp.location}<br/>
+                            <small>{exp.period}</small><br/>
+                            {exp.desc}
+                        </p>
+                    </div>
+                    <nav className="level is-mobile">
+                        <div className="level-left">
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </div>;
+    }
+}
+
+export default ProjectBox;
