e360f24823ae9d9d078d086471021e6ac117979a
user <sh.daewoong@navercorp.com>
2019-12-05 14:14:37 +0900

    react test

---
+++ v6/build/precache-manifest.28f67f65b7b982e174c2fd74f1a6c0ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a29992804175dc4781763327c886456",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ddce4db355c3ae8c9b1",
+    "url": "/static/css/main.ca99be7e.chunk.css"
+  },
+  {
+    "revision": "ddd7689b79de38e8a6a6",
+    "url": "/static/js/2.eb042d73.chunk.js"
+  },
+  {
+    "revision": "0ddce4db355c3ae8c9b1",
+    "url": "/static/js/main.7e352e27.chunk.js"
+  },
+  {
+    "revision": "b3d3ea9826b226520f58",
+    "url": "/static/js/runtime-main.0b418aa0.js"
+  }
+]);
